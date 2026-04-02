178229bd90fbdd7e5b96e42e7f23d1a4cee7f278
suresh1247 <suresh1247jb@gmail.com>
2026-04-02 05:51:39 +0000

    Clean up inventory configuration in install_aap.sh
    Removed redundant hostname configuration lines from inventory.

--- install_aap.sh
+++ install_aap.sh
@@ -19,6 +19,10 @@ subscription-manager repos --enable=rhel-10-for-x86_64-baseos-rpms
 subscription-manager repos --enable=rhel-10-for-x86_64-appstream-rpms
 
 dnf install -y ansible-core wget git-core rsync
+rm -rf /etc/firewalld
+yum reinstall -y firewalld
+systemctl start firewalld
+systemctl enable firewalld
 
 su - admin -c "cd /home/admin && wget $3 -O aap.tar.gz"
 su - admin -c "cd /home/admin && tar -xvzf aap.tar.gz"
@@ -26,13 +30,5 @@ su - admin -c "cd /home/admin && tar -xvzf aap.tar.gz"
 INVENTORY_PATH="/home/admin/ansible-automation-platform-containerized-setup-2.6-6/inventory"
 
 sed -i "s/RHEL-10.example.com/$NEW_HOSTNAME/g" $INVENTORY_PATH
-
-grep -q "^automationcontroller_hostnames=" $INVENTORY_PATH && \
-sed -i "s/^automationcontroller_hostnames=.*/automationcontroller_hostnames=$NEW_HOSTNAME/" $INVENTORY_PATH || \
-echo "automationcontroller_hostnames=$NEW_HOSTNAME" >> $INVENTORY_PATH
-
-echo "automationhub_hostnames=$NEW_HOSTNAME" >> $INVENTORY_PATH
-echo "automationgateway_hostnames=$NEW_HOSTNAME" >> $INVENTORY_PATH
-echo "automationeda_hostnames=$NEW_HOSTNAME" >> $INVENTORY_PATH
 
 su - admin -c "cd /home/admin/ansible-automation-platform-containerized-setup-2.6-6 && ansible-playbook -i inventory ansible.containerized_installer.install"
